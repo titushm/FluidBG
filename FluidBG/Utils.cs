6422096c20b2b93eb8c900145a479be6e3cdb240
titushm <titusmeader@btinternet.com>
2025-01-03 14:52:15 +0000

    - Organised code - Adjusted window size to be smaller - Removed duplicate "startup" header in settings tab - Added primary method to get spotlight image (more random). Official api will be used as fallback. - Fixed bug that would cause non image files being set as background when picking from folder. - Separate tile button added. - wallpaper modes and tile now have an effect on the wallpaper. - All settings will be correctly set to their saved state when loading app.

---
+++ FluidBG/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace FluidBG;
+
+public static class Constants {
+    public static readonly Version Version = new Version(1, 0, 9);
+    public static readonly string GithubRepoUrl = "https://github.com/titushm/FluidBG";
+    public static readonly int[] ComboBoxSecondIntervals = { 1, 60, 3600, 86400, 604800 };
+    public static RegistryKey StartupRegisteryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+    
+    public struct Paths {
+        public static readonly string DataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\titushm\\FluidBG";
+        public static readonly string LogFile = $"{DataFolder}\\log.tmp";
+        public static readonly string ConfigFile = $"{DataFolder}\\config.json";
+    }
+
+    public static Dictionary<int, int> WallpaperModes = new(){
+        {0, 10},
+        {1, 6},
+        {2, 2},
+        {3, 0},
+        {4, 0},
+        {5, 22}
+    };
+}
