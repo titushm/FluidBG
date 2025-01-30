c658939447d3f0bab55c82854ce686da1054be6f
titushm <titusmeader@btinternet.com>
2025-01-30 10:23:27 +0000

    actually final bugfix, wont crash when the datafolder directory does not exist or when config properties are missing

--- FluidBG/Utils.cs
+++ FluidBG/Utils.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using Microsoft.Win32;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -138,7 +139,7 @@ public class Utils {
             JsonConvert.DeserializeObject<JObject>(jsonString);
         }
         catch {
-            File.WriteAllText(Constants.Paths.ConfigFile, "{}");
+            File.WriteAllText(Constants.Paths.ConfigFile, "{\"sourcePaths\":[]}");
         }
     }
     
